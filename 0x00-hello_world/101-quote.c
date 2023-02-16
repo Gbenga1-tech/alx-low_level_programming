99105263892b8a6f5780bcd576fabc1faaeb702f
Gbenga1-tech <yasirgbenga313@gmail.com>
2023-02-16 15:40:34 -0800

    without printf and puts

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
-#include<unistd>
+#include<unistd.h>
 /**
- * main - entry point
+ * main - is entry point
  *
- * Return: 1,if succesful
+ * Return: 1, if succesful
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
