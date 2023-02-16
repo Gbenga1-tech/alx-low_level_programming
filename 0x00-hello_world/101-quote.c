2c70f5b3406c6197138f944020d61b4ec16dcd90
Gbenga1-tech <yasirgbenga313@gmail.com>
2023-02-16 15:34:43 -0800

    without printf or puts

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include<unistd>
+/**
+ * main - entry point
+ *
+ * Return: 1,if succesful
+ */
+int main(void)
+{
+	write("and that piece of art is useful" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
