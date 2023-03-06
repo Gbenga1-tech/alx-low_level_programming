bf1a8e1dc071a8a5e811f5bfea4bd44eadf18be3
Gbenga1-tech <yasirgbenga313@gmail.com>
2023-03-06 13:58:52 -0800

    a function that sets the value of a pointer to a char

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,27 +1,11 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
- * print_diagsums - Prints the sum of the two diagonals
- *                  of a square matrix of integers.
- * @a: The matrix of integers.
- * @size: The size of the matrix.
+ * set_string - Sets the value of a pointer to a char.
+ * @s: The pointer.
+ * @to: The char.
  */
-
-
-void print_diagsums(int *a, int size)
+void set_string(char **s, char *to)
 {
-	int i, n, sum1 = 0, sum2 = 0;
-
-	for (i = 0; i <= (size * size); i = i + size + 1)
-	{
-		sum1 = sum1 + a[i];
-	}
-
-	for (n = size - 1; n <= (size * size) - size; n = n + size - 1)
-	{
-		sum2 = sum2 + a[n];
-	}
-
-	printf("%d, %d\n", sum1, sum2);
+	*s = to;
 }
