286ce57c6b6367883e540b9518ff3c0281817776
Gbenga1-tech <yasirgbenga313@gmail.com>
2023-04-04 10:17:55 -0700

    task 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -2,14 +2,14 @@
 #include <stdlib.h>
 
 /**
- * free_listint2 - frees a listnt_t list5-free_listnt2.c
+ * free_listint2 - frees a listint_t list5-free_listint2.c.
  * @head: a double pointer to the head of the list
  *
  * Return: void
  */
 void free_listint2(listint_t **head);
 {
-	listint_t *temp
+	listint_t *temp;
 
 	if (head == NULL)
 		return;
