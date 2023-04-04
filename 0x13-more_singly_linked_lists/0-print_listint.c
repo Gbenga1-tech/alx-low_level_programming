a0e30e4c6c1347ba65cef64ec52a30372d37411b
Gbenga1-tech <yasirgbenga313@gmail.com>
2023-04-04 04:45:16 -0700

    task

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,18 +9,13 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t  node_count = 1;
+	size_t nodes = 0;
 
-	if (h == NULL)
-		return (0);
-
-	while (h->next != NULL)
+	while (h != NULL)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
-		node_count++;
+		nodes++;
 	}
-	printf("%d\n", h->n);
-
-	return (node_count);
+	return (nodes);
 }
